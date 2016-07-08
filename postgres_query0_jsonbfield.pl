9f5b50f699a6d854e631d6ddb51cc25d7d27a334
smb528 <smb528@cornell.edu>
2016-07-08 17:03:04 +0000

    added scripts

---
+++ postgres_query0_jsonbfield.pl
@@ -0,0 +1,79 @@
+#!/usr/bin/perl
+
+
+
+use DBI;
+use strict;
+use Data::Dumper;
+use JSON;
+use Time::HiRes;
+use Getopt::Std;
+
+our ($opt_H, $opt_D, $opt_p, $opt_m);
+
+getopts('H:D:p:m:');
+
+if (!$opt_H || !$opt_D || !$opt_p || !$opt_m) {
+    die "Must provide options -H (hostname), -D (database name), -p (db user password), -m (protocol name) \n";
+}
+
+my $dbhost = $opt_H;
+my $dbname = $opt_D;
+my $dbpass = $opt_p;
+my $protocol_name = $opt_m;
+
+my $start = Time::HiRes::time();
+
+my $driver = "Pg";
+my $dsn = "DBI:$driver:dbname=$dbname;host=$dbhost;port=5432";
+my $userid = "postgres";
+my $password = $dbpass;
+my $dbh = DBI->connect($dsn,$userid, $password, {RaiseError => 1}) or die $DBI::errstr;
+
+		       print "Opened database successfully\n";
+
+my $sth = $dbh->prepare("select json_object_keys((genotypeprop.value ->> 'GT')::json) from nd_experiment join nd_experiment_genotype using(nd_experiment_id) join genotype using(genotype_id) join genotypeprop using(genotype_id) join nd_experiment_protocol using(nd_experiment_id) join nd_protocol using(nd_protocol_id) WHERE nd_protocol.name = ?;")
+    or die "Couldn't prepare statement: " . $dbh->errstr;
+
+$sth->execute($protocol_name);
+
+my $json = JSON->new();
+
+print STDERR Dumper "Number Genotypeprops: ".$sth->rows();
+
+while(my $row=$sth->fetchrow_arrayref){
+
+    my $mutations_count = 0;
+    my $ref_count = 0;
+    my $bad_quality = 0;
+
+    print STDERR Dumper $row;
+
+    my $json_value = $json->decode(@$row);
+    #print Dumper $json_value;
+
+    foreach my $marker (keys %$json_value) {
+	my $GT = $json_value->{$marker}->{'GT'};
+	my $GQ = $json_value->{$marker}->{'GQ'};
+
+	if ($GQ > 90) {
+	    if ($GT eq '0/0') {
+		$mutations_count++;
+	    } else {
+		$ref_count++;
+	    }
+	} else {
+	    $bad_quality++;
+	}
+    }
+
+    #print "BAD COUNT: ".$bad_quality."\n";
+    #print "MUTATIONS COUNT: ".$mutations_count."\n";
+    #print "REF COUNT: ".$ref_count."\n\n";
+
+}
+
+my $end = Time::HiRes::time();
+my $duration = $end - $start;
+
+print Dumper "Time: ".$duration;
--- query2.pl
+++ query2.pl
@@ -7,81 +7,112 @@ use strict;
 use Data::Dumper;
 use JSON;
 use Time::HiRes;
+use Getopt::Std;
+use List::MoreUtils qw(first_index);
+
+our ($opt_H, $opt_D, $opt_p, $opt_m, $opt_c, $opt_n);
+
+getopts('H:D:p:m:c:n:');
+
+if (!$opt_H || !$opt_D || !$opt_p || !$opt_m || !$opt_n) {
+    die "Must provide options -H (hostname), -D (database name), -p (db user password), -m (protocol name), -n (num reps) \n";
+}
+
+my $dbhost = $opt_H;
+my $dbname = $opt_D;
+my $dbpass = $opt_p;
+my $protocol_name = $opt_m;
+my $count_deletion_in_either_chr = $opt_c || 0;
+my $num_reps = $opt_n;
 
 my $start = Time::HiRes::time();
 
 my $driver = "Pg";
-my $database = "fixture";
-my $dsn = "DBI:$driver:dbname=$database;host=127.0.0.1;port=5432";
+my $dsn = "DBI:$driver:dbname=$dbname;host=$dbhost;port=5432";
 my $userid = "postgres";
-my $password = "postgres";
+my $password = $dbpass;
 my $dbh = DBI->connect($dsn,$userid, $password, {RaiseError => 1}) or die $DBI::errstr;
-
 		       print "Opened database successfully\n";
-
-my $sth = $dbh->prepare('SELECT value FROM public.genotypeprop order by genotypeprop_id DESC limit 11;')
-    or die "Couldn't prepare statement: " . $dbh->errstr;
-
-my $rth = $dbh->prepare('SELECT value FROM public.nd_protocolprop order by nd_protocolprop_id DESC limit 11;')
-    or die "didn't work: " . $dbh->errstr;
-
-$sth->execute();
-$rth->execute();
 
 my $json = JSON->new();
 
-my %unique_alts;
+for (0..$num_reps) {
 
-while(my $row=$sth->fetchrow_arrayref, my$row2=$rth->fetchrow_arrayref){
+		my $sth = $dbh->prepare('select nd_protocolprop.value, genotypeprop.value from nd_experiment join nd_experiment_genotype using(nd_experiment_id) join genotype using(genotype_id) join genotypeprop using(genotype_id) join nd_experiment_protocol using(nd_experiment_id) join nd_protocol using(nd_protocol_id) join nd_protocolprop using(nd_protocol_id) where nd_protocol.name = ?')
+				or die "Couldn't prepare statement: " . $dbh->errstr;
 
+		$sth->execute($protocol_name);
 
-    my $deletion_count = 0;
-    my $nondeletion_count = 0;
+		my %unique_alts;
 
-    my $json_value = $json->decode(@$row);
-    #print Dumper $json_value;
-    my $json_value2 = $json-> decode(@$row2);
-		#print Dumper $json_value2;
+		while(my ($protocol_json, $genotype_json) = $sth->fetchrow_array){
 
+		    my $deletion_count = 0;
+		    my $nondeletion_count = 0;
 
-    foreach my $marker (keys %$json_value ) {
-	my $GT = $json_value->{$marker}->{'GT'};
-	my $GQ = $json_value->{$marker}->{'GQ'};
-	my $alt = $json_value2->{$marker}->{'alt'};
+		    my $protocol_json_value = $json->decode($protocol_json);
+				#print Dumper $protocol_json_value;
+				my $genotype_json_value = $json->decode($genotype_json);
+		    #print Dumper $genotype_json_value;
 
-		$unique_alts{$alt} = $GT;
+				foreach my $marker (keys %$protocol_json_value ) {
 
-		  if ($alt eq '-' and $GT ne '0/0') {
+						my $alt = $protocol_json_value->{$marker}->{'alt'};
+						my $GT = $genotype_json_value->{$marker}->{'GT'};  ## '1/1', '1/3'
+						#my $GQ = $genotype_json_value->{$marker}->{'GQ'};
 
-		      $deletion_count++;
-		  } elsif ($alt eq 'A,-' and $GT ne '0/0' and $GT ne '1/1') {
-		      $deletion_count++;
-		  } elsif ($alt eq '-,A' and $GT ne '0/0' and $GT ne '2/2') {
-		      $deletion_count++;
-		  } elsif ($alt eq '-,T' and $GT ne '0/0' and $GT ne '2/2') {
-                      $deletion_count++;
-		  } elsif ($alt eq 'T,-' and $GT ne '0/0' and $GT ne '1/1') {
-		      $deletion_count++;
-                  } elsif ($alt eq 'G,-' and $GT ne '0/0' and $GT ne '1/1') {
-		      $deletion_count++;
-		  } elsif ($alt eq '-,G' and $GT ne '0/0' and $GT ne '2/2') {
-		      $deletion_count++;
-		  } elsif ($alt eq 'C,-' and $GT ne '0/0' and $GT ne '1/1') {
-		      $deletion_count++;
-		  } elsif ($alt eq '-,C' and $GT ne '0/0' and $GT ne '2/2') {
-		      $deletion_count++;
-				  } else {
+						#print Dumper $alt;
+						#print Dumper $GT;
+
+						#$unique_alts{$alt} = $GT;
+
+						if ($alt =~ /-/ && $GT ne '0/0') {
+
+							my @alt_array = split /,/, $alt;
+							#print Dumper \@alt_array;
+
+							my $del_index = first_index { $_ eq '-' } @alt_array;
 
 
-				      $nondeletion_count++;
-				  } }
+							if ($count_deletion_in_either_chr) {
+									if($GT =~ /$del_index/) {
+										 $deletion_count++;
+									}
+									else {
+										$nondeletion_count++;
+									}
 
+							}
+							else {
 
-    print "DELETIONS COUNT: ".$deletion_count."\n";
-    print "NONDELETION COUNT: ".$nondeletion_count."\n";
+									my $del_mutation = "$del_index/$del_index";
+
+									if ($GT eq $del_mutation) {
+											#print Dumper $del_mutation;
+											#print Dumper $GT;
+											$deletion_count++;
+									}
+									else {
+										#print Dumper $del_mutation;
+										#print Dumper $GT;
+										$nondeletion_count++;
+									}
+
+							}
+
+						}
+						else {
+								$nondeletion_count++;
+						}
+
+				}
+
+				print "DELETIONS COUNT: ".$deletion_count."\n";
+		    print "NONDELETION COUNT: ".$nondeletion_count."\n";
+		}
+
+		#print Dumper \%unique_alts;
 }
-
-print Dumper \%unique_alts;
 
 my $end = Time::HiRes::time();
 my $duration = $end - $start;
