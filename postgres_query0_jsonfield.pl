569446476256f5890c9c2c66fe084f5ede920429
nickmorales <nmorales3142@gmail.com>
2016-07-12 14:41:08 +0200

    added json query0 script

---
+++ postgres_query0_jsonfield.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/perl
+
+
+
+use DBI;
+use strict;
+use Data::Dumper;
+use JSON;
+use Time::HiRes;
+use Getopt::Std;
+
+our ($opt_H, $opt_D, $opt_p, $opt_m);
+
+getopts('H:D:p:m:');
+
+if (!$opt_H || !$opt_D || !$opt_p || !$opt_m) {
+    die "Must provide options -H (hostname), -D (database name), -p (db user password), -m (protocol name) \n";
+}
+
+my $dbhost = $opt_H;
+my $dbname = $opt_D;
+my $dbpass = $opt_p;
+my $protocol_name = $opt_m;
+
+my $start = Time::HiRes::time();
+
+my $driver = "Pg";
+my $dsn = "DBI:$driver:dbname=$dbname;host=$dbhost;port=5432";
+my $userid = "postgres";
+my $password = $dbpass;
+my $dbh = DBI->connect($dsn,$userid, $password, {RaiseError => 1}) or die $DBI::errstr;
+
+		       print "Opened database successfully\n";
+
+#Get List of all Stocks that were geotypes using the protocol given.
+my $sth = $dbh->prepare("SELECT stock.stock_id from stock join nd_experiment_stock using(stock_id) join nd_experiment using(nd_experiment_id) join nd_experiment_protocol using(nd_experiment_id) join nd_protocol using(nd_protocol_id) where nd_protocol.name = ?;")
+    or die "Couldn't prepare statement: " . $dbh->errstr;
+
+$sth->execute($protocol_name);
+
+
+#Loop over the list of stocks that we found.
+while (my $stock_id = $sth->fetchrow_array) {
+
+  #Count markers where GT is not 0/0 for an individual stock.
+  my $sth_count = $dbh->prepare("select count(kv.key) from stock join nd_experiment_stock using(stock_id) join nd_experiment using(nd_experiment_id) join nd_experiment_genotype using(nd_experiment_id) join genotype using(genotype_id) join genotypeprop AS a using(genotype_id), json_each(a.value) kv WHERE stock.stock_id = $stock_id and not kv.value @> ? ;")
+      or die "Couldn't prepare statement: " . $dbh->errstr;
+
+  #print $stock_id;
+
+  $sth_count->execute('{"GT":"0/0"}');
+  my $mutations_count = $sth_count->fetchrow_array();
+
+  print "MUTATIONS COUNT: ".$mutations_count."\n";
+
+}
+
+my $end = Time::HiRes::time();
+my $duration = $end - $start;
+
+print Dumper "Time: ".$duration;
